2026c44117e810f75cde91b0ed7ab0d06ebc9979
Ken-ichi Yamada <ke-yamada@sakura.ad.jp>
2024-10-14 14:38:28 +0900

    libcamera-still

--- upload_image_to_slack.sh
+++ upload_image_to_slack.sh
@@ -7,19 +7,18 @@ IMAGE_PATH="/tmp/image.jpg"  # 画像の保存先を /tmp に設定
 COMMENT="Photo taken at $(date)!"  # 撮影時刻をコメントに含める
 
 # 画像を撮影（画像の保存先は /tmp/image.jpg）
-raspistill -o "$IMAGE_PATH" \
-           -w 4056 -h 3040 \
-           -hf -vf \
-           -ex auto \
-           -awb auto \
-           -q 100 \
-           -sh 40 \
-           -br 60 \
-           -co 10 \
-           -ISO 100 \
-           -drc high \
-           -a 12 -a "Photo taken at %Y-%m-%d %X" \
-           -sa 0
+libcamera-still -o /tmp/image.jpg \
+                --width 4056 --height 3040 \  # 最大解像度（12MP）
+                --rotation 180 \              # 必要に応じて回転
+                --sharpness 1.0 \             # シャープネスの強調
+                --brightness 0.5 \            # 明るさ調整
+                --contrast 1.0 \              # コントラスト調整
+                --gain 1.0 \                  # ゲイン（感度）
+                --saturation 1.0 \            # 彩度調整
+                --ev 0 \                      # 露出補正
+                --timeout 1000 \              # タイムアウト
+                --hdr \                       # HDRを有効にする
+                --metadata                    # メタデータを含める
 
 # Slackに画像をアップロードするAPIリクエスト
 curl -F file=@"$IMAGE_PATH" \
